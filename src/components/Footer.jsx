0abacfe2da35689f0ecb7c179f62b3e21ef5136c
Jainil09 <pateljainil91@gmail.com>
2024-12-24 22:31:05 +0530

    Updated

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer className="bg-gray-900 text-white p-4 mt-10 text-center">
-        <p>&copy; 2024 Splitwise</p>
-    </footer>
-  )
-}
-
-export default Footer
